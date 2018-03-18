a712367839742f3275a023be896a18cde31c7f46
lqb1996 <lqb1139134758@163.com>
2018-03-18 16:55:04 +0800

    login

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      */
     protected $table = 'users';
     protected $fillable = [
-        'nickName', 'openId',
+        'nickName', 'openId', 'gender', 'language', 'city', 'province', 'country', 'avatarUrl'
     ];
 
     /**
